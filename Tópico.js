d3891cddbe1a118ba4a029e8bf5d9c9038b6108d
Apps <wallxnip@gmail.com>
2025-09-26 12:38:36 -0300

    Criar Tópico.js

---
+++ Tópico.js
@@ -0,0 +1,4 @@
+const playlist2 = [
+  { title: "Música 4", artist: "Artista D", file: "audio/track4.mp3", cover: "covers/cover4.jpg" },
+  { title: "Música 5", artist: "Artista E", file: "audio/track5.mp3", cover: "covers/cover5.jpg" },
+];
