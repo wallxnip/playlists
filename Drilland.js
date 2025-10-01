ec1370e81e82b38093e9460e333813bfe3571b0e
Apps <wallxnip@gmail.com>
2025-10-01 00:14:30 -0300

    Atualizar o Drilland.js

--- Drilland.js
+++ Drilland.js
@@ -1,1 +1,17 @@
-t
+// Lista de músicas
+const playlist3 = [
+  {
+    title: "MOOD",
+    artist: "Drilland (Melodic Drill Type Beat)",
+    file: "https://www.voxnaija.com.ng/wp-content/uploads/2023/09/Drilland_-_Mood_Melodic_Drill_Type_Beat_.mp3",
+    cover: "https://www.voxnaija.com.ng/wp-content/uploads/2023/09/Drilland-%E2%80%93-Mood-Melodic-Drill-Type-Beat.webp"
+  },
+  {
+    title: "Ghost",
+    artist: "Drilland",
+    file: "https://www.thinkmusic.com.ng/thinknews/uploads/2025/06/Justin_Bieber_-_Ghost_thinknews.com.ng.mp3",
+    cover: "https://i.postimg.cc/fb2GMsFZ/Ghost.jpg"
+  }
+  
+
+];
