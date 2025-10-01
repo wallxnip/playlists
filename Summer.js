175324e5a95e767b72ade3d4e88a10024b03e2ef
Apps <wallxnip@gmail.com>
2025-10-01 19:25:56 -0300

    Atualizar o Summer.js

--- Summer.js
+++ Summer.js
@@ -42,11 +42,61 @@ const playlist20 = [
     cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
   },
 
+  {
+    title: "Deserto Rain",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Desert%20Rain.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Alive",
+    artist: "Summer (MIX)",
+    file: "https://ia600300.us.archive.org/14/items/good-feeling_202510/Alive.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Holiday",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Holiday.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Infinity",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Infinity%202008.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Love Again",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Love%20Again.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Play Hard",
+    artist: "Summer (MIX)",
+    file: "https://ia800300.us.archive.org/14/items/good-feeling_202510/Play%20Hard.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
+
+  {
+    title: "Player In C",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Prayer%20in%20C.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
+
+  {
+    title: "Stereo Love",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Stereo%20Love.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
 
 
