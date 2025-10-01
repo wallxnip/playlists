bd9bd0968a705d6267f3094ba1a4928bcfe76ac1
Apps <wallxnip@gmail.com>
2025-10-01 18:42:14 -0300

    Atualizar o Summer.js

--- Summer.js
+++ Summer.js
@@ -1,1 +1,28 @@
-g
+// Lista de músicas
+const playlist19 = [
+  {
+    title: "MOOD",
+    artist: "Drilland (Melodic Drill Type Beat)",
+    file: "https://dn721905.ca.archive.org/0/items/humor_20251001/MOOD.mp3",
+    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+  },
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
