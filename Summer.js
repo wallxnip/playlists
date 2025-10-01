379b398a5b90122ef237975a69e57a1247fe7b34
Apps <wallxnip@gmail.com>
2025-10-01 19:11:44 -0300

    Atualizar o Summer.js

--- Summer.js
+++ Summer.js
@@ -7,10 +7,40 @@ const playlist20 = [
     cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
   },
   
+  {
+    title: "Hight Again",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/High%20Again.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Good Feeling",
+    artist: "Summer (MIX)",
+    file: "https://ia800300.us.archive.org/14/items/good-feeling_202510/Good%20Feeling.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Girls in Luv",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Girls%20in%20Luv.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Summer",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Summer.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
+  {
+    title: "Fall In Love",
+    artist: "Summer (MIX)",
+    file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/Fall%20In%20Love.mp3",
+    cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
+  },
 
 
 
