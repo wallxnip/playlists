60faf3a35ceded777bdada94178b99af72f4a216
Apps <wallxnip@gmail.com>
2025-10-01 18:56:40 -0300

    Atualizar o Summer.js

--- Summer.js
+++ Summer.js
@@ -1,7 +1,7 @@
 // Lista de músicas
 const playlist20 = [
   {
-    title: "MOOD",
+    title: "So Good",
     artist: "Summer (MIX)",
     file: "https://dn720706.ca.archive.org/0/items/good-feeling_202510/So%20Good.mp3",
     cover: "https://i.postimg.cc/J4rFvrmT/8.jpg"
