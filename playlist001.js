6e7593747fa929ab691e6a981d57000ea0f2e158
Apps <wallxnip@gmail.com>
2025-10-02 04:21:46 -0300

    Atualizar o playlist001.js

--- playlist001.js
+++ playlist001.js
@@ -4,37 +4,37 @@ const playlist1 = [
     title: "Sorry",
     artist: "Justin Bieber",
     file: "https://files.catbox.moe/gm6wog.mp3",
-    cover: "https://i.postimg.cc/fTC0zgw5/Just-Bieber.jpg"
+    cover: "https://i.postimg.cc/Qdrs1YC5/Pin-by-Emma-on-Singers-Justin-bieber-wallpaper-Justin-bieb.png"
   },
   {
     title: "Ghost",
     artist: "Justin Bieber",
     file: "https://files.catbox.moe/82i0xx.mp3",
-    cover: "https://i.postimg.cc/fb2GMsFZ/Ghost.jpg"
+    cover: "https://i.postimg.cc/gjgSnxLJ/Stream-Justin-Bieber-Ghost-by-Kixibee-Listen-online-for-free.jpg"
   },
   {
     title: "Kiss Me",
     artist: "Dermot Kennedy",
     file: "https://files.catbox.moe/8zb396.mp3",
-    cover: "https://i.postimg.cc/ydCFgg2T/Kiss-Me.jpg"
+    cover: "https://i.postimg.cc/V6MvCyNt/Dermot-Kennedy-Kiss-Me-Acoustic-Lyrics-Genius-Lyrics.jpg"
   },
   {
     title: "Better Day",
     artist: "Dermot Kennedy",
     file: "https://files.catbox.moe/s6iin3.mp3",
-    cover: "https://i.postimg.cc/ydCFgg2T/Kiss-Me.jpg"
+    cover: "https://i.postimg.cc/V6MvCyNt/Dermot-Kennedy-Kiss-Me-Acoustic-Lyrics-Genius-Lyrics.jpg"
   },
   {
     title: "Numb",
     artist: "Marshmello, Khalid",
     file: "https://files.catbox.moe/u9n81t.mp3",
-    cover: "https://i.postimg.cc/kg6j1Njy/Numb.jpg"
+    cover: "https://i.postimg.cc/02GS4SPS/Apple-Music-Marshmello-amp-Khalid-Numb-Single-1.jpg"
   },
   {
     title: "Girls in Luv",
     artist: "DJane HouseKat feat. Rameez",
     file: "https://files.catbox.moe/0etv0s.mp3",
-    cover: "https://i.postimg.cc/kgFj3Yy7/Girls-in-Luv.jpg"
+    cover: "https://i.postimg.cc/wvSNCc1X/Girls-in-Luv-feat-DJane-House-Kat-You-Tube-Music-1.jpg"
   },
   {
     title: "Good Feeling",
@@ -46,62 +46,62 @@ const playlist1 = [
     title: "Whistle",
     artist: "Flo Rida",
     file: "https://files.catbox.moe/clnap6.mp3",
-    cover: "https://i.postimg.cc/XNTvCs6K/Whistle.png"
+    cover: "https://i.postimg.cc/LsQLTMR0/Flo-Rida-Whistle-Lyrics-Genius-Lyrics.png"
   },
   {
     title: "Hear Me Now",
     artist: "Alok,feat Zeeba",
     file: "https://files.catbox.moe/pixm40.mp3",
-    cover: "https://i.postimg.cc/3NKWkRvy/Alok.jpg"
+    cover: "https://i.postimg.cc/htgL4rpM/Alok-Wallpapers-4k-HD-Alok-Backgrounds-on-Wallpaper-Bat.jpg"
   },
   {
     title: "There For Me",
     artist: "Braaten & Victor Perry",
     file: "https://files.catbox.moe/55rhhy.mp3",
-    cover: "https://i.postimg.cc/ZYMhBn31/There-For-Me.png"
+    cover: "https://i.postimg.cc/2jsdMmWs/Braaten-amp-Victor-Perry-There-For-Me-Lyrics-Genius-Lyrics.png"
   },
   {
     title: "Burn",
     artist: "Ellie Goulding",
     file: "https://files.catbox.moe/nagm5w.mp3",
-    cover: "https://i.postimg.cc/XYD2xBNb/Ellie-Goulding-Burn.png"
+    cover: "https://i.postimg.cc/bN10jMHs/Ellie-Goulding-Burn-Album.png"
   },
   {
     title: "She Doesn t Mind",
     artist: "Sean Paul",
     file: "https://files.catbox.moe/2sihut.mp3",
-    cover: "https://i.postimg.cc/DwsgQjNq/She-Doesn-t-Mind.jpg"
+    cover: "https://i.postimg.cc/B6tc9nM0/Corrientes-de-M-sica-SEAN-PAUL-She-Doesn-t-Mind-Remix.jpg"
   },
   {
     title: "What The Hell",
     artist: "Avril Lavigne",
     file: "https://files.catbox.moe/3xrm6t.mp3",
-    cover: "https://i.postimg.cc/Vkmy2VCF/Avril-Lavigne-What-the-Hell.jpg"
+    cover: "https://i.postimg.cc/Vsp4wnxH/Image-gallery-for-Avril-Lavigne-What-the-Hell-Music-Video-Fi.jpg"
   },
   {
     title: "Girl On The Moon",
     artist: "Naya",
     file: "https://files.catbox.moe/l25lw2.mp3",
-    cover: "https://i.postimg.cc/fR93gkYd/Naya-Ruby.jpg"
+    cover: "https://i.postimg.cc/SN9dxjqQ/Naya-une-girl-on-the-moon-prometteuse-interview-Ruby-1.jpg"
   },
   
   {
     title: "One More Night",
     artist: "Lost Frequencies",
     file: "https://files.catbox.moe/0yy4cy.mp3",
-    cover: "https://i.postimg.cc/4NcswY0c/One-More-Night.jpg"
+    cover: "https://i.postimg.cc/vm4cGxF3/Lost-Frequencies-Unveils-New-Track-039-One-More-Night-039-EDMTunes.jpg"
   },
   {
     title: "Dont Leave Me Now",
     artist: "Lost Frequencies",
     file: "https://files.catbox.moe/w6cggl.mp3",
-    cover: "https://i.postimg.cc/9Fst5n1S/Lzf.jpg"
+    cover: "https://i.postimg.cc/sg6MF98M/PAST-EVENTS-Theory-Elevengg.jpg"
   },
   {
     title: "Play",
     artist: "Alan Walker & Tungevaag",
     file: "https://files.catbox.moe/1s4gxi.mp3",
-    cover: "https://i.postimg.cc/qRMCCPp1/AW.jpg"
+    cover: "https://i.postimg.cc/cCRvVqyW/61-Alan-walker-ideas-alan-walker-walker-alany.jpg"
   },
   {
     title: "Knockout",
@@ -125,43 +125,43 @@ const playlist1 = [
     title: "Tusa",
     artist: "Karol",
     file: "https://files.catbox.moe/udl6zt.mp3",
-    cover: "https://i.postimg.cc/XJ2z062F/Karol-G.jpg"
+    cover: "https://i.postimg.cc/15SnDzw3/Karol-G-Feat-Nicki-Minaj-Tusa-David-Dancos-Remix-by-David-D.jpg"
   },
   {
     title: "Tired",
     artist: "Alan Walker",
     file: "https://files.catbox.moe/52a0nh.mp3",
-    cover: "https://i.postimg.cc/pXvq1TV7/Tired.jpg"
+    cover: "https://i.postimg.cc/TPtZNfj0/Beauty-Plus-20251002041642052-save.jpg"
   },
   {
     title: "One More Light",
     artist: "Linkin Park",
     file: "https://files.catbox.moe/rnom3s.mp3",
-    cover: "https://i.postimg.cc/W3M0MGZ4/Linkin-Park-One-More-Light.jpg"
+    cover: "https://i.postimg.cc/8kMfFvFY/Linkin-Park-One-More-Light-2017-be-subjective-1.jpg"
   },
   {
     title: "Apocalypse",
     artist: "Cigarette After",
     file: "https://files.catbox.moe/gmsls0.mp3",
-    cover: "https://i.postimg.cc/nhFyQ263/Apocalypse.jpg"
+    cover: "https://i.postimg.cc/PqJ8NFZb/Cigarettes-After-Sex-Apocalypse-Lyrics-Genius-Lyrics.jpg"
   },
   {
     title: "Dynasty",
     artist: "Miia",
     file: "https://files.catbox.moe/n4pgnt.mp3",
-    cover: "https://i.postimg.cc/Z5PDjNhC/Miia.jpg"
+    cover: "https://i.postimg.cc/FK6kSggM/1758591260433.jpg"
   },
   {
     title: "Say Yes To Heaven",
     artist: "Lana Del Rey",
     file: "https://files.catbox.moe/q7o25y.mp3",
-    cover: "https://i.postimg.cc/Jz9Zy6FX/Yes-To-Heaven.png"
+    cover: "https://i.postimg.cc/t42xqH1C/Lana-Del-Rey-Yes-To-Heaven-by-Kallum-Lavigne-on-Deviant-Art.png"
   },
   {
     title: "Royalty",
     artist: "Egzod, Maestro Chives, Neoni",
     file: "https://files.catbox.moe/9ok0rh.mp3",
-    cover: "https://i.postimg.cc/FKpSPrRy/Royalty.jpg"
+    cover: "https://i.postimg.cc/v8VfWjK3/Egzod-on-TIDAL.jpg"
   },
   {
     title: "Lovely",
