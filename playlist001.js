dd9cba4a8f3326e8644d88f615079a232091ed59
Apps <wallxnip@gmail.com>
2025-10-04 15:57:43 -0300

    Atualizar o playlist001.js

--- playlist001.js
+++ playlist001.js
@@ -5,6 +5,7 @@ const playlist1 = [
     artist: "Justin Bieber",
     file: "https://files.catbox.moe/gm6wog.mp3",
     cover: "https://i.postimg.cc/Qdrs1YC5/Pin-by-Emma-on-Singers-Justin-bieber-wallpaper-Justin-bieb.png"
+    coverBlur: "https://i.postimg.cc/XXXXX/pixel-borrado.jpg"
   },
   {
     title: "Ghost",
