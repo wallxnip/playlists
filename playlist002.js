0648da51b0527105e918162ca7b8fad48f3c95fc
Apps <wallxnip@gmail.com>
2025-10-06 15:14:35 -0300

    Criar playlist002.js

---
+++ playlist002.js
@@ -0,0 +1,11 @@
+// Lista de músicas
+const playlist2 = [
+  {
+    title: "Quando posso te ver novamente?",
+    artist: "Detona Ralph",
+    file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/cover.pnh"
+  },
+
+
+];
