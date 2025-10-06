ac6ebf643b7eaa32d1a5fba781eb8f819c2b3341
Apps <wallxnip@gmail.com>
2025-10-06 15:21:38 -0300

    Atualizar o playlist002.js

--- playlist002.js
+++ playlist002.js
@@ -4,7 +4,7 @@ const playlist002 = [
     title: "Quando posso te ver novamente?",
     artist: "Detona Ralph",
     file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
-    cover: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/cover.pnh"
+    cover: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/cover.png"
   },
 
 
