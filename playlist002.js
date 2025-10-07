2431daf577d92ceba5235152ae3c5f292d1da3ca
Apps <wallxnip@gmail.com>
2025-10-07 02:43:34 -0300

    Atualizar o playlist002.js

--- playlist002.js
+++ playlist002.js
@@ -6,6 +6,23 @@ const playlist2 = [
     file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
     cover: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/cover.png"
   },
-
+  {
+    title: "Civilian",
+    artist: "The Walking Dead",
+    file: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/music.m4a",
+    cover: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/cover.png"
+  },
+  {
+    title: "Serpents",
+    artist: "The Walking Dead",
+    file: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/music.m4a",
+    cover: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/cover.png"
+  },
+  {
+    title: "The Last Pale Light In the West",
+    artist: "The Walking Dead",
+    file: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/music.m4a",
+    cover: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/cover.png"
+  },
 
 ];
