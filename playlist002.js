d7f54295b55e16dee3bde94fb3d1e26249ac9eca
Apps <wallxnip@gmail.com>
2025-10-08 16:01:42 -0300

    Atualizar o playlist002.js

--- playlist002.js
+++ playlist002.js
@@ -4,25 +4,25 @@ const playlist2 = [
     title: "Quando posso te ver novamente?",
     artist: "Detona Ralph",
     file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
-    cover: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/cover.png"
+    cover: "https://i.postimg.cc/rFnxMb7N/RD.webp"
   },
   {
     title: "Civilian",
     artist: "The Walking Dead",
     file: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/music.m4a",
-    cover: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/cover.png"
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
   },
   {
     title: "Serpents",
     artist: "The Walking Dead",
     file: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/music.m4a",
-    cover: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/cover.png"
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
   },
   {
     title: "The Last Pale Light In the West",
     artist: "The Walking Dead",
     file: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/music.m4a",
-    cover: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/cover.png"
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
   },
 
 ];
