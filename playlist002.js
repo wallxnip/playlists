b75d28a191136f62c1eb1d7c67675c0106b312ed
Apps <wallxnip@gmail.com>
2025-10-13 20:32:33 -0300

    Atualizar o playlist002.js

--- playlist002.js
+++ playlist002.js
@@ -1,28 +1,55 @@
-// Trilha Sonora
-const playlist005 = [
+// Lista de músicas
+const playlist002 = [
   {
-    title: "Quando posso te ver novamente?",
-    artist: "Detona Ralph",
-    file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
-    cover: "https://i.postimg.cc/rFnxMb7N/RD.webp"
+    title: "That's Who I Praise",
+    artist: "Brandon Lake",
+    file: "https://hs10.krakenfiles.com/uploads/13-10-2025/2OhVBcU9Tl/music.m4a",
+    cover: "https://i.postimg.cc/76hgnk4H/That-s-Who-I-Praise.webp"
   },
   {
-    title: "Civilian",
-    artist: "The Walking Dead",
-    file: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/music.m4a",
-    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+    title: "Anyone",
+    artist: "Justin Bieber",
+    file: "https://hs2.krakenfiles.com/uploads/13-10-2025/bSF16U7sM9/music.m4a",
+    cover: "https://i.postimg.cc/tJ0Nqc0b/Anyone.webp"
   },
   {
-    title: "Serpents",
-    artist: "The Walking Dead",
-    file: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/music.m4a",
-    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+    title: "Control",
+    artist: "Romee Storm",
+    file: "https://hs10.krakenfiles.com/uploads/13-10-2025/dcxKtmVF6E/music.m4a",
+    cover: "https://i.postimg.cc/g0CZ86g1/Control.webp"
   },
   {
-    title: "The Last Pale Light In the West",
-    artist: "The Walking Dead",
-    file: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/music.m4a",
-    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+    title: "Hall of Fame",
+    artist: "The Script, William",
+    file: "https://hs1.krakenfiles.com/uploads/13-10-2025/fmu1gx9jTU/music.m4a",
+    cover: "https://i.postimg.cc/cL6gzcjj/Hall-of-Fame.webp"
   },
-
+  {
+    title: "Slip Away",
+    artist: "Johnny Valentine",
+    file: "https://hs7.krakenfiles.com/uploads/13-10-2025/129xWLdcjk/music.m4a",
+    cover: "https://i.postimg.cc/NMxjFySR/Slip-Away.webp"
+  },
+  {
+    title: "Stay",
+    artist: "The Kid Laroi, Justin Bieberr",
+    file: "https://hs7.krakenfiles.com/uploads/13-10-2025/hoRpw00dWw/music.m4a",
+    cover: "https://i.postimg.cc/4xtZn7Hr/Stay.webp"
+  },
+  {
+    title: "The Nights",
+    artist: "Avicii",
+    file: "https://hs5.krakenfiles.com/uploads/13-10-2025/R6452N4hEk/music.m4a",
+    cover: "https://i.postimg.cc/ht9WckH4/The-Nights.webp"
+  },
+  {
+    title: "Way Back Home",
+    artist: "Cassiopeia",
+    file: "https://hs3.krakenfiles.com/uploads/13-10-2025/x7kwoglh31/music.m4a",
+    cover: "https://i.postimg.cc/fbBPbDCG/Way-Back-Home.webp"
+  },
+  
+  
+  
+  
 ];
