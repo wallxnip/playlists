f4188630ce383871b1d0b0aad180b27507b7ba8d
Apps <wallxnip@gmail.com>
2025-10-13 20:33:32 -0300

    Criar playlist005.js

---
+++ playlist005.js
@@ -0,0 +1,28 @@
+// Trilha Sonora
+const playlist005 = [
+  {
+    title: "Quando posso te ver novamente?",
+    artist: "Detona Ralph",
+    file: "https://hs7.krakenfiles.com/uploads/06-10-2025/tILcWWjSIc/music.m4a",
+    cover: "https://i.postimg.cc/rFnxMb7N/RD.webp"
+  },
+  {
+    title: "Civilian",
+    artist: "The Walking Dead",
+    file: "https://hs10.krakenfiles.com/uploads/07-10-2025/zOT5cyh8lI/music.m4a",
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+  },
+  {
+    title: "Serpents",
+    artist: "The Walking Dead",
+    file: "https://hs5.krakenfiles.com/uploads/07-10-2025/xxI5XNA6cK/music.m4a",
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+  },
+  {
+    title: "The Last Pale Light In the West",
+    artist: "The Walking Dead",
+    file: "https://hs6.krakenfiles.com/uploads/07-10-2025/iuxzKX6D5l/music.m4a",
+    cover: "https://i.postimg.cc/1tv5Dx0Q/TWD.webp"
+  },
+
+];
