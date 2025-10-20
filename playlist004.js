03d15173970ec1a71c5edcf1371c7985dc8bc0db
Apps <wallxnip@gmail.com>
2025-10-20 06:53:45 -0300

    Atualizar o playlist004.js

--- playlist004.js
+++ playlist004.js
@@ -12,5 +12,65 @@ const playlist004 = [
     file: "https://hs4.krakenfiles.com/uploads/14-10-2025/tggc9z0iRJ/music.m4a",
     cover: "https://i.postimg.cc/Gm5TGhrK/Os-Mlk-Liso.webp"
   },
+  {
+    title: "Tudo de Bom",
+    artist: "Mc Livinho",
+    file: "https://hs8.krakenfiles.com/uploads/20-10-2025/mAjjRtM5tr/music.m4a",
+    cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
+  },
+  {
+    title: "Perito no Ofício",
+    artist: "MC Boy do Charmes",
+    file: "https://hs7.krakenfiles.com/uploads/20-10-2025/Du9wPfwJDk/music.m4a",
+    cover: "https://i.postimg.cc/MG28fC7b/boy.webp"
+  },
+  {
+    title: "Nosso Amor",
+    artist: "MC Pedrinho",
+    file: "https://hs8.krakenfiles.com/uploads/20-10-2025/16DGgVayS1/music.m4a",
+    cover: "https://i.postimg.cc/q7Vd6Ps5/Pedrinho.webp"
+  },
+  {
+    title: "Maria Mariah",
+    artist: "Luanzinho, MC Meno Dani",
+    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/sQ5jVgDguJ/music.m4a",
+    cover: "https://i.postimg.cc/FsCSvZyh/mariah.webp"
+  },
+  {
+    title: "Lei do Retorno",
+    artist: "Mc Don Juan, MC Hariel",
+    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/xBt9bZB4W2/music.m4a",
+    cover: "https://i.postimg.cc/Vkb86fCH/don.webp"
+  },
+  {
+    title: "Fazer Falta",
+    artist: "Mc Livinho, Perera DJ",
+    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/OFFuUiI80K/music.m4a",
+    cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
+  },
+  {
+    title: "Cheia de Marra",
+    artist: "Mc Livinho",
+    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/UYTEBZtE6h/music.m4a",
+    cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
+  },
+  {
+    title: "Cavalo de Tróia",
+    artist: "Mc Kevin",
+    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/H289ys2at1/music.m4a",
+    cover: "https://i.postimg.cc/MG28fC7N/Mc-Kevin.webp"
+  },
+  {
+    title: "Azul Piscina",
+    artist: "Mc Livinho",
+    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/5TrtvbHcgP/music.m4a",
+    cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
+  },
+  {
+    title: "Amar Amei",
+    artist: "Mc Don Juan",
+    file: "https://hs7.krakenfiles.com/uploads/20-10-2025/jEovx8CYkV/music.m4a",
+    cover: "https://i.postimg.cc/jSJb5x73/juan.webp"
+  },
 
 ];
