1787928526d8a5ac53abe17fdd3a2cb97cc2036b
Apps <wallxnip@gmail.com>
2025-11-29 15:59:48 -0300

    Atualizar o playlist004.js

--- playlist004.js
+++ playlist004.js
@@ -1,76 +1,81 @@
-// Lista de músicas
+// Funk (Deluxe)
 const playlist004 = [
   {
     title: "Iluminado",
     artist: "Kawe",
-    file: "https://hs5.krakenfiles.com/uploads/10-10-2025/f9Dv2W9H49/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Iluminado.mp3",
     cover: "https://i.postimg.cc/hGYLsWKT/Iluminado.webp"
   },
   {
     title: "Os Muleke E Liso",
     artist: "MC Rodolfinho",
-    file: "https://hs4.krakenfiles.com/uploads/14-10-2025/tggc9z0iRJ/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Os Muleke É Liso.mp3",
     cover: "https://i.postimg.cc/Gm5TGhrK/Os-Mlk-Liso.webp"
   },
   {
     title: "Maria Mariah",
     artist: "Luanzinho, MC Meno Dani",
-    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/sQ5jVgDguJ/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Maria Mariah.mp3,
     cover: "https://i.postimg.cc/FsCSvZyh/mariah.webp"
   },
   {
     title: "Tudo de Bom",
     artist: "Mc Livinho",
-    file: "https://hs8.krakenfiles.com/uploads/20-10-2025/mAjjRtM5tr/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Tudo de Bom.mp3",
     cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
   },
   {
     title: "Perito no Ofício",
     artist: "MC Boy do Charmes",
-    file: "https://hs7.krakenfiles.com/uploads/20-10-2025/Du9wPfwJDk/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Perito no Ofício.mp3",
     cover: "https://i.postimg.cc/MG28fC7b/boy.webp"
   },
   {
     title: "Nosso Amor",
     artist: "MC Pedrinho",
-    file: "https://hs8.krakenfiles.com/uploads/20-10-2025/16DGgVayS1/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Nosso Amor.mp3",
     cover: "https://i.postimg.cc/q7Vd6Ps5/Pedrinho.webp"
   },
   {
     title: "Lei do Retorno",
     artist: "Mc Don Juan, MC Hariel",
-    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/xBt9bZB4W2/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Lei do Retorno.mp3",
     cover: "https://i.postimg.cc/Vkb86fCH/don.webp"
   },
   {
     title: "Fazer Falta",
     artist: "Mc Livinho, Perera DJ",
-    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/OFFuUiI80K/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Fazer Falta.mp3",
     cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
   },
   {
     title: "Cheia de Marra",
     artist: "Mc Livinho",
-    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/UYTEBZtE6h/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Cheia de Marra.mp3",
     cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
   },
   {
     title: "Cavalo de Tróia",
     artist: "Mc Kevin",
-    file: "https://hs5.krakenfiles.com/uploads/20-10-2025/H289ys2at1/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Cavalo de Tróia.mp3",
     cover: "https://i.postimg.cc/MG28fC7N/Mc-Kevin.webp"
   },
   {
     title: "Azul Piscina",
     artist: "Mc Livinho",
-    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/5TrtvbHcgP/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Azul Piscina.mp3",
     cover: "https://i.postimg.cc/bw2hJytB/Livinho.webp"
   },
   {
     title: "Amar Amei",
     artist: "Mc Don Juan",
-    file: "https://hs7.krakenfiles.com/uploads/20-10-2025/jEovx8CYkV/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Amar Amei.mp3",
     cover: "https://i.postimg.cc/jSJb5x73/juan.webp"
   },
-
+  {
+    title: "Cachorrinha de Madame",
+    artist: "DJ Duarte, DJ TS, Mc Gw, Pollini, Prime Funk",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (Deluxe)/Cachorrinha de Madame.mp3",
+    cover: "https://i.postimg.cc/jSJb5x73/juan.webp"
+  }
 ];
