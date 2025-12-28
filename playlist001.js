7c31b14d9d78e3830ddb80e250edcf6329899060
Apps <wallxnip@gmail.com>
2025-12-28 06:56:40 -0300

    Atualizar o playlist001.js

--- playlist001.js
+++ playlist001.js
@@ -1,225 +1,177 @@
 // Lançamentos
 const playlist1 = [
   {
-    title: "Pandora",
-    artist: "David Win",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Pandora.mp3",
-    cover: "https://i.postimg.cc/br7hWvrz/Pandora.webp"
+    title: "Giuliana",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Giuliana.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
-    title: "I Regret You",
-    artist: "Bree Taylor",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/I Regret You.mp3",
-    cover: "https://i.postimg.cc/vm4dY9ns/BT.webp"
+    title: "Vaqueira",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Vaqueira.mp3",
+    cover: "https://lh3.googleusercontent.com/wGlM3Iy2hpWgMuj7ZrL_py3XGOAERS7nm1w1g3eyLe2hNOnVf9olYIwrkqS5qzzymouQxnXMRrgHfFhD=w544-h544-l90-rj"
   },
   {
-    title: "Sorry",
-    artist: "Justin Bieber",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Sorry.mp3",
-    cover: "https://i.postimg.cc/BvB2vLGt/Sorry.webp"
+    title: "190",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/190.mp3",
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
-    title: "Ghost",
-    artist: "Justin Bieber",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Ghost.mp3",
-    cover: "https://i.postimg.cc/zG177R5k/ghost.webp"
+    title: "Samara",
+    artist: "Cintura de Mola",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Cintura de Mola/Samara.mp3",
+    cover: "https://lh3.googleusercontent.com/7UDZtu3gz2KuD0B27MZr7pzdlIl5xRjKRYeUx5YiVS-Gr7UoKPqDS7fYdPhPfvKclwS9zBwCawyW6f_V=w544-h544-l90-rj"
   },
   {
-    title: "Kiss Me",
-    artist: "Dermot Kennedy",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Kiss Me.mp3",
-    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
+    title: "Aquele Love",
+    artist: "Grego do Piseiro",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Aquele Love.mp3",
+    cover: "https://lh3.googleusercontent.com/fGWWEBHU2QOFRFxkEzR482sU3jV1V9H1DdF-4uQukD1yT6_DgWqTfqqcieEySaNyABgFVY1CKkX6xEtV=w544-h544-l90-rj"
   },
   {
-    title: "Better Days",
-    artist: "Dermot Kennedy",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Better Days.mp3",
-    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
+    title: "Porta aberta",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Porta aberta.mp3",
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
   },
   {
-    title: "Numb",
-    artist: "Marshmello, Khalid",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Numb.mp3",
-    cover: "https://i.postimg.cc/y8FQCyvN/Apple-Music-Marshmello-Khalid-Numb-Single-1-2.webp"
+    title: "Olhar 43",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Olhar 43.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
-    title: "Girls in Luv",
-    artist: "DJane HouseKat feat. Rameez",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Girls in Luv.mp3",
-    cover: "https://i.postimg.cc/Jz16mBg6/Girls-in-Luv.webp"
+    title: "Você é Mó Bagunçada",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Você é mó bagunçada.mp3",
+    cover: "https://lh3.googleusercontent.com/A5P8syU_D0iwghdja2Xt43DM-izKGXjHH3DiPpSiccYMT82J53pNHT_idV49bJw_indjrU3P-4dpMyII=w544-h544-l90-rj"
   },
   {
-    title: "Good Feeling",
-    artist: "Flo Rida",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Good Feeling.mp3",
-    cover: "https://i.postimg.cc/QM7Wyfb6/FD.webp"
+    title: "Sou Louco Por Ela",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Sou Louco Por Ela.mp3",
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
-    title: "Whistle",
-    artist: "Flo Rida",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Whistle.mp3",
-    cover: "https://i.postimg.cc/hvYDgB4D/FRW.webp"
+    title: "Me Deixa Doidona",
+    artist: "Cintura de Mola",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Cintura de Mola/Me Deixa Doidona.mp3",
+    cover: "https://lh3.googleusercontent.com/7UDZtu3gz2KuD0B27MZr7pzdlIl5xRjKRYeUx5YiVS-Gr7UoKPqDS7fYdPhPfvKclwS9zBwCawyW6f_V=w544-h544-l90-rj"
   },
   {
-    title: "Hear Me Now",
-    artist: "Alok,feat Zeeba",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Hear Me Now.mp3",
-    cover: "https://i.postimg.cc/ydk6Gxsg/Alok.webp"
+    title: "Pane no Sistema",
+    artist: "Grego do Piseiro & Paulo Pires",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Pane no Sistema.mp3",
+    cover: "https://lh3.googleusercontent.com/CwFnVlWDyolBLFyrzRX9JEdE52gmzdjDKGG3wotLNb8hISPOWFrmHJ-Nm5fkMH94T36-apTnvykwTos6Dw=w544-h544-l90-rj"
   },
   {
-    title: "Blinding Lights",
-    artist: "The Weeknd",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Blinding Lights.mp3",
-    cover: "https://i.postimg.cc/bwCKBTJG/TWB.webp"
+    title: "Tô nem aí",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Tô nem aí.mp3",
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
   },
   {
-    title: "Das Geht Boom",
-    artist: "Harris & Ford, Gordon & Doyle, Lisah",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Das Geht Boom.mp3",
-    cover: "https://i.postimg.cc/wxrR9KGQ/hz.webp"
+    title: "A Cada Dez Palavras",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/A Cada Dez Palavras.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
-    title: "From Up Above",
-    artist: "Twinjabi",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/From Up Above.mp3",
-    cover: "https://i.postimg.cc/6503TDHg/Twinjabi.webp"
+    title: "Pensa Direito",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Pensa direito.mp3",
+    cover: "https://lh3.googleusercontent.com/wGlM3Iy2hpWgMuj7ZrL_py3XGOAERS7nm1w1g3eyLe2hNOnVf9olYIwrkqS5qzzymouQxnXMRrgHfFhD=w544-h544-l90-rj"
   },
   {
-    title: "Greedy",
-    artist: "Tate McRae",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Greedy.mp3",
-    cover: "https://i.postimg.cc/SxPmMqc7/GY.webp"
+    title: "Batom Vermelho",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Batom Vermelho.mp3",
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
-    title: "Hislerim",
-    artist: "Serhat Durmus, Zerrin",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Hislerim.mp3",
-    cover: "https://i.postimg.cc/W1BQV4GN/HZ.webp"
+    title: "Menina Pavoro",
+    artist: "Cintura de Mola",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Cintura de Mola/Menina Pavorô.mp3",
+    cover: "https://lh3.googleusercontent.com/7UDZtu3gz2KuD0B27MZr7pzdlIl5xRjKRYeUx5YiVS-Gr7UoKPqDS7fYdPhPfvKclwS9zBwCawyW6f_V=w544-h544-l90-rj"
   },
   {
-    title: "Ma Chérie",
-    artist: "DJ Antoine, The Beatshakers",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Ma Chérie.mp3",
-    cover: "https://i.postimg.cc/hG5zLB41/DJA.webp"
+    title: "Cheiro de Mato",
+    artist: "Gabi Saiury, Kotim & MC Paiva ZS",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Saiury/Cheiro de Mato.mp3",
+    cover: "https://lh3.googleusercontent.com/3Z3v-mLTZ99X48pJbEqZg0njWCqtM3jLyAZsmfGaZLo6NHNf0JCNHnpJ4srXFYN-080rriauclckziI=w544-h544-l90-rj"
   },
   {
-    title: "There For Me",
-    artist: "Braaten & Victor Perry",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/There For Me.mp3",
-    cover: "https://i.postimg.cc/QdPhSx5b/There-For-Me.webp"
+    title: "Céu de Diamantes",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Céu de Diamantes.mp3",
+    cover: "https://i.postimg.cc/QMCdBpM5/luka.webp"
   },
   {
-    title: "Burn",
-    artist: "Ellie Goulding",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Burn.mp3",
-    cover: "https://i.postimg.cc/j28G5Z4g/Ellie-Goulding-Burn-Album.webp"
+    title: "A Dor Desse Amor",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/A Dor Desse Amor.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
-    title: "She Doesn't Mind",
-    artist: "Sean Paul",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/She Doesn't Mind.mp3",
-    cover: "https://i.postimg.cc/nzFRDqz7/Sean-Paul-She-Doesn-t-Mind-Dj-Pamen-Remix.webp"
+    title: "Fabricada na Roça",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Saiury/Fabricada na Roça.mp3",
+    cover: "https://lh3.googleusercontent.com/8dpVCNgAmSWgf8ZjSbV6Bl-po-8Lqgjiix20Nvp5ixIUudK6fPmc4LogoMu6Y-6WllW5ysHTG9pSzUTPDQ=w544-h544-l90-rj"
   },
   {
-    title: "What The Hell",
-    artist: "Avril Lavigne",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/What The Hell.mp3",
-    cover: "https://i.postimg.cc/4yK16PT9/wth.webp"
+    title: "Na Linha do Tempo",
+    artist: "Victor & Leo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Na Linha do Tempo.mp3",
+    cover: "https://i.postimg.cc/Ssh3BdCh/Na-Linha-do-Tempo.webp"
   },
   {
-    title: "Girl On The Moon",
-    artist: "Naya",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Girl On The Moon.mp3",
-    cover: "https://i.postimg.cc/PqNHyC5w/Naya-Ruby.webp"
+    title: "Querendo ou Não",
+    artist: "Gabi Saiury, Japinha Conde & Jonatas Nascimento",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Saiury/Querendo ou Não.mp3",
+    cover: "https://lh3.googleusercontent.com/OyyaVucCCKttbJj8bsm-VDhd1MOX9eHDL7vTvmXhU7Sja3Qcaim3ZgjRmcFtcCyA5uqRO1ntgRHvbQQ=w544-h544-l90-rj"
   },
   {
-    title: "One More Night",
-    artist: "Lost Frequencies",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/One More Night.mp3",
-    cover: "https://i.postimg.cc/JzPVd7kS/omn.webp"
+    title: "Destino",
+    artist: "Lucas Lucco",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Destino.mp3",
+    cover: "https://i.postimg.cc/wTZfKFY2/Destino.webp"
   },
   {
-    title: "Don't Leave Me Now",
-    artist: "Lost Frequencies",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Don't Leave Me Now.mp3",
-    cover: "https://i.postimg.cc/zv0VfZQH/Lost-Frequencies.webp"
+    title: "Farra É Pra Quem Pode",
+    artist: "Cintura de Mola",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Cintura de Mola/Farra É Pra Quem Pode.mp3",
+    cover: "https://lh3.googleusercontent.com/7UDZtu3gz2KuD0B27MZr7pzdlIl5xRjKRYeUx5YiVS-Gr7UoKPqDS7fYdPhPfvKclwS9zBwCawyW6f_V=w544-h544-l90-rj"
   },
   {
-    title: "Play",
-    artist: "Alan Walker & Tungevaag",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Play.mp3",
-    cover: "https://i.postimg.cc/43jJB4DF/Play.webp"
+    title: "Duas",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Duas.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "Knockout",
-    artist: "Tungevaag",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Knockout.mp3",
-    cover: "https://i.postimg.cc/KYfPMVCT/1000479222-2.webp"
+    title: "Nosso Quadro",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Nosso Quadro.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "Make You Happy",
-    artist: "Tungevaag",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Make You Happy.mp3",
-    cover: "https://i.postimg.cc/KYfPMVCT/1000479222-2.webp"
+    title: "Casa de Milhões",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Casa de Milhões.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "Kingdoms",
-    artist: "Tungevaag & Jay Hardway",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Kingdoms.mp3",
-    cover: "https://i.postimg.cc/KYfPMVCT/1000479222-2.webp"
+    title: "Depois do Universo",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Depois do Universo.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "Tusa",
-    artist: "Karol",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Tusa.mp3",
-    cover: "https://i.postimg.cc/W16f1s6Y/Tusa.webp"
+    title: "Chuva",
+    artist: "João Bosco & Vinicius",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Chuva.mp3",
+    cover: "https://i.postimg.cc/t4fHMF2K/Chuva.webp"
   },
-  {
-    title: "Tired",
-    artist: "Alan Walker",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Tired.mp3",
-    cover: "https://i.postimg.cc/d3fcnwSD/Tired.webp"
-  },
-  {
-    title: "One More Light",
-    artist: "Linkin Park",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/One More Light.mp3",
-    cover: "https://i.postimg.cc/MHHvvZQ8/One-More-Light.webp"
-  },
-  {
-    title: "Apocalypse",
-    artist: "Cigarette After",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Apocalypse.mp3",
-    cover: "https://i.postimg.cc/9QdFXwnT/Apocalypse.webp"
-  },
-  {
-    title: "Dynasty",
-    artist: "Miia",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Dynasty.mp3",
-    cover: "https://i.postimg.cc/Gtjnwv2J/Dynasty.webp"
-  },
-  {
-    title: "Say Yes To Heaven",
-    artist: "Lana Del Rey",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Say Yes To Heaven.mp3",
-    cover: "https://i.postimg.cc/Pxh6G98c/Yes-To-Heaven.webpg"
-  },
-  {
-    title: "Royalty",
-    artist: "Egzod, Maestro Chives, Neoni",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Royalty.mp3",
-    cover: "https://i.postimg.cc/8CbRv5jd/Egzod-on-TIDAL.webp"
-  },
-  {
-    title: "Lovely",
-    artist: "Billie Eilish",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Lovely.mp3",
-    cover: "https://i.postimg.cc/J41Gyqbr/Beauty-Plus-20251008155100417-save-1.webp"
-  },
-  {
-    title: "Let Me Down Slowly",
-    artist: "Alec Benjamin",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Let Me Down Slowly.mp3",
-    cover: "https://i.postimg.cc/3wjsDgXC/LMDS.webp"
-  }
 ];
